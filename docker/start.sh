ddb66429b514095a168a53fc5c1277cbed630d32
asolda <a.solda@studenti.unisa.it>
2017-08-30 19:52:29 +0100

    Docker scripts and Dockerfile

---
+++ docker/start.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+set -e
+
+# Handle special flags if we're root
+if [ $(id -u) == 0 ] ; then
+    # Handle username change. Since this is cheap, do this unconditionally
+    usermod -d /home/$NB_USER -l $NB_USER jovyan
+
+    # Change UID of NB_USER to NB_UID if it does not match
+    if [ "$NB_UID" != $(id -u $NB_USER) ] ; then
+        echo "Set user UID to: $NB_UID"
+        usermod -u $NB_UID $NB_USER
+    fi
+
+    # Change GID of NB_USER to NB_GID if NB_GID is passed as a parameter
+    if [ "$NB_GID" ] ; then
+        echo "Change GID to $NB_GID"
+        groupmod -g $NB_GID -o $(id -g -n $NB_USER)
+    fi
+
+    # Enable sudo if requested
+    if [[ "$GRANT_SUDO" == "1" || "$GRANT_SUDO" == 'yes' ]]; then
+        echo "Granting $NB_USER sudo access"
+        echo "$NB_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook
+    fi
+
+    # Exec the command as NB_USER
+    echo "Execute the command as $NB_USER"
+    exec su $NB_USER -c "env PATH=$PATH $*"
+else
+  if [[ ! -z "$NB_UID" && "$NB_UID" != "$(id -u)" ]]; then
+      echo 'Container must be run as root to set $NB_UID'
+  fi
+  if [[ ! -z "$NB_GID" && "$NB_GID" != "$(id -g)" ]]; then
+      echo 'Container must be run as root to set $NB_GID'
+  fi
+  if [[ "$GRANT_SUDO" == "1" || "$GRANT_SUDO" == 'yes' ]]; then
+      echo 'Container must be run as root to grant sudo permissions'
+  fi
+    # Exec the command
+    echo "Execute the command"
+    exec $*
+fi
